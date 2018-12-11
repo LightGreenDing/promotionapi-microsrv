12237055962dae36d476a917ae4d09d20e519f87
丁杰 <496328388@qq.com>
2018-12-11 09:31:36 +0800

    Initial commit

---
+++ src/main/java/com/wowokuaiyun/promotionapimicrosrv/PromotionapiMicrosrvApplication.java
@@ -0,0 +1,12 @@
+package com.wowokuaiyun.promotionapimicrosrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionapiMicrosrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromotionapiMicrosrvApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wowokuaiyun/promotionapimicrosrv/PromotionapiMicrosrvApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wowokuaiyun.promotionapimicrosrv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PromotionapiMicrosrvApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
